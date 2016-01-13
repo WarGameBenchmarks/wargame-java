9f1f68a4f8709e4e6689eec177c295e51aa9bd36
Ryan Rampersad <ryan.rampersad@gmail.com>
2016-01-13 01:45:06 -0600

    Added new WarGame
    - App temporarily fires WarGame
    - Benchmark was deleted
    - Card was changed split into 3, Value and Suit also

--- source/wargame/App.java
+++ source/wargame/App.java
@@ -4,11 +4,10 @@ import java.text.*;
 
 class App {
 
-	public static final long ms = Benchmark.MS;
-	public static final long ns = Benchmark.NS;
-
 	public static void main(String[] args) {
 
+		WarGame game = new WarGame();
+		game.play();
 
 	}
 
--- source/wargame/WarGame.java
+++ source/wargame/WarGame.java
@@ -1,11 +1,69 @@
 package wargame;
 
-import java.util.*;
-
 class WarGame {
 
-  public void game() {
-    
+  public void play() {
+
+    Deck deck = Deck.fresh();
+    deck.shuffle();
+
+    Deck[] player = deck.split();
+
+    Deck winner = new Deck();
+
+    base:
+    while (player[0].hasCards() && player[1].hasCards()) {
+
+
+      Card c1 = player[0].getCard();
+      Card c2 = player[1].getCard();
+
+      player[0].giveCard(winner);
+      player[1].giveCard(winner);
+
+      if (c1.compareTo(c2) == 0) {
+
+        war: do {
+          if (player[0].getSize() < 4 || player[1].getSize() < 4) {
+            break base;
+          }
+
+          for (int i = 0; i < 3; i++) {
+            player[0].giveCard(winner);
+            player[1].giveCard(winner);
+          }
+
+          c1 = player[0].getCard();
+          c2 = player[1].getCard();
+
+          player[0].giveCard(winner);
+          player[1].giveCard(winner);
+
+          if (c1.compareTo(c2) > 0) {
+            winner.shuffle();
+            winner.giveCards(player[0]);
+          } else if (c1.compareTo(c2) < 0) {
+            winner.shuffle();
+            winner.giveCards(player[1]);
+          } else {
+            // another war
+          }
+
+        } while (c1.compareTo(c2) == 0);
+
+      } else if (c1.compareTo(c2) > 0) {
+        winner.shuffle();
+        winner.giveCards(player[0]);
+      } else if (c1.compareTo(c2) < 0) {
+        winner.shuffle();
+        winner.giveCards(player[1]);
+      }
+
+    }
+
+    System.out.printf("p1 %d - p2 %d\n", player[0].getSize(), player[1].getSize());
+    // System.out.println("testing");
+
   }
 
 }
